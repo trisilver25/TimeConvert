17f4883a3faf73f729b43b9b7fa27512c475e702
Tristin Smith <tsmithassoc@gmail.com>
2024-05-31 07:33:16 -0400

    Initial Commit

---
+++ TimeConvert/TimeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConvertApp.swift
+//  TimeConvert
+//
+//  Created by Tristin Smith on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
