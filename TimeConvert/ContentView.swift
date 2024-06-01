d05d0000a5236e6cec33a4b7ef841ea8410a7d1b
Tristin Smith <tsmithassoc@gmail.com>
2024-05-31 21:36:05 -0400

    Initial Push: - Added 6 properties. - Created a navigationStack inside it is a form, inside that is 4 sections, 1 for input, 2 for selecting values, and 1 for output. - Computed property convertedTime converts any of the time values.

--- TimeConvert/ContentView.swift
+++ TimeConvert/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var timeAmount = 0.0
+    @State private var fromTimeValue = "Seconds"
+    @State private var toTimeValue = "Seconds"
+    
+    let timeValues = ["Seconds", "Minutes", "Hours", "Days"]
+    
+    var convertedTime: Double {
+        var convertedValue = 0.0
+        
+        if fromTimeValue == toTimeValue {
+            convertedValue = timeAmount
+        } else if fromTimeValue == "Seconds" && toTimeValue == "Minutes" {
+            convertedValue = timeAmount / 60
+        } else if fromTimeValue == "Seconds" && toTimeValue == "Hours" {
+            convertedValue =  (timeAmount / 60) / 60
+        } else if fromTimeValue == "Seconds" && toTimeValue == "Days" {
+            convertedValue = ((timeAmount / 60) / 60) / 24
+        } else if fromTimeValue == "Minutes" && toTimeValue == "Seconds" {
+            convertedValue = timeAmount * 60
+        } else if fromTimeValue == "Minutes" && toTimeValue == "Hours" {
+            convertedValue = timeAmount / 60
+        } else if fromTimeValue == "Minutes" && toTimeValue == "Days" {
+            convertedValue = (timeAmount / 60) / 24
+        } else if fromTimeValue == "Hours" && toTimeValue == "Seconds" {
+            convertedValue = (timeAmount * 60) * 60
+        } else if fromTimeValue == "Hours" && toTimeValue == "Minutes" {
+            convertedValue = timeAmount * 60
+        } else if fromTimeValue == "Hours" && toTimeValue == "Days" {
+            convertedValue = timeAmount / 24
+        } else if fromTimeValue == "Days" && toTimeValue == "Seconds" {
+            convertedValue = ((timeAmount * 24) * 60) * 60
+        } else if fromTimeValue == "Days" && toTimeValue == "Minutes" {
+            convertedValue = (timeAmount * 24) * 60
+        } else if fromTimeValue == "Days" && toTimeValue == "Hours" {
+            convertedValue = timeAmount * 24
         }
-        .padding()
+        
+        return convertedValue
+    }
+    
+    var formatValue: String {
+        let value = convertedTime
+        return value.formatted(.number)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Convert From") {
+                    Picker("Convert From", selection: $fromTimeValue){
+                        ForEach(timeValues, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Enter Value") {
+                    TextField("Time amount", value: $timeAmount, format: .number)
+                    }
+                Section("Convert To"){
+                    Picker("Convert To", selection: $toTimeValue) {
+                        ForEach(timeValues, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Converted Value") {
+                    Text("\(formatValue) \(toTimeValue)")
+                }
+            }
+            .navigationTitle("TimeConvert")
+        }
     }
 }
 
